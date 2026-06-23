d5d7316c581cb4db82843fe33643ce67116f6d38
luisalberto26 <56483785+luisalberto26@users.noreply.github.com>
2026-06-22 22:40:40 -0400

    Add Celsius to Fahrenheit conversion program
    This program converts Celsius to Fahrenheit using the formula F = (C × 9/5) + 32 and displays the results in the console.

---
+++ Far-to-celsius.js
@@ -0,0 +1,11 @@
+// Este programa convierte grados Celsius a Fahrenheit.
+//Fórmula: F = (C × 9/5) + 32
+// Se declara la variable celsius como número y se calcula fahrenheit.
+// Finalmente se muestran los resultados en consola.
+
+let celsius = 30; // Number
+let fahrenheit = (celsius * 9/5) + 32; // Number
+
+console.log("Celsius: " + celsius);
+console.log("Resultado: " + fahrenheit + "°F");
+
